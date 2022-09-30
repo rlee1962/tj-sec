5a0c946cfce2d5141870e14fadb0f2ba3d97d857
R. Lee <rlee1962@gmail.com>
2022-09-29 18:45:44 -0600

    added close button to modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,16 +1,51 @@
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import ReactDom from 'react-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+import { RiCloseCircleLine, RiCloseCircleFill } from 'react-icons/ri';
+import { AiOutlineClose } from 'react-icons/ai';
 
 // import styles from '../styles/Home.module.css';
 
 function Modal({ open, children, onClose }) {
+  const router = useRouter();
+  console.log(onClose);
+
   if (!open) return null;
   return ReactDom.createPortal(
     <>
-      <div className="modal-overlay" onClick={onClose} />
-      <div className="modal-body">
-        <button onClick={onClose}>Close Modal</button> {children}
-      </div>
+      <AnimatePresence>
+        <motion.div
+          onClick={onClose}
+          className="modal-overlay"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+          }}
+          exit={{
+            opacity: 0,
+            transition: { duration: 3, delay: 0.25 },
+          }}
+        />
+
+        <motion.div
+          className="modal-body"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { duration: 2, delay: 0.25 },
+          }}
+          exit={{
+            opacity: 0,
+            transition: { duration: 3, delay: 0.25 },
+          }}
+        >
+          {/* <button onClick={onClose}>Close Modal</button>  */}
+          {children}
+
+          <AiOutlineClose onClick={onClose} className="modal-close-button" />
+        </motion.div>
+      </AnimatePresence>
     </>,
     document.getElementById('modal-root'),
   );
--- components/featured/ImageText.jsx
+++ components/featured/ImageText.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const ImageText = () => {
   return (
     <div>
-      <p className="text-[64px] capitalize">Image Text</p>
+      <p className="text-[64px] capitalize  text-gray-100">Image Text</p>
     </div>
   );
 };
